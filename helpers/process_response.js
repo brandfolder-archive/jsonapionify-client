21c7c570a5c61626970b58ee13c8ba092b2b1ea9
Jason Waldrip <jason@waldrip.net>
2016-01-29 15:56:17 -0700

    better message formatting

--- helpers/process_response.js
+++ helpers/process_response.js
@@ -7,7 +7,17 @@ module.exports = function processResponse(response) {
     return new Promise(function (resolve, reject) {
         if (response.json && response.json.errors) {
             var message = response.json.errors.map(function (error) {
-                return _.compact([ error.status, error.title, ':', error.detail ]).join(' ')
+                var msg = '';
+                if (error.status) {
+                    msg += error.status
+                }
+                if (error.title) {
+                    msg += msg ? ` ${error.title}` : error.title
+                }
+                if (error.detail) {
+                    msg += msg ? `: ${error.detail}` : error.detail
+                }
+                return msg
             }).join(', ');
             reject(new HTTPError[ `_${response.status}` ](message), response);
         } else {
