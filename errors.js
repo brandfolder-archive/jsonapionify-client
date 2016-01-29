2539955a3ec5c8db2261e92f8c60c946ee3dc31c
Jason Waldrip <jason@waldrip.net>
2016-01-29 15:48:24 -0700

    use actual args

--- errors.js
+++ errors.js
@@ -1,8 +1,8 @@
 "use strict";
 
 class HTTPError extends Error {
-    constructor(...args) {
-        super(...args);
+    constructor(message, fileName, lineNumber) {
+        super(message, fileName, lineNumber);
         this.name = this.constructor.name;
         Error.captureStackTrace(this, this.constructor.name)
     }
@@ -14,12 +14,15 @@ for (var i = 400; i <= 599; i++) {
 
         if (i % 100 == 0) {
             HTTPError[ baseClassName ] = class extends HTTPError {
+                constructor(message, fileName, lineNumber) {
+                    super(message, fileName, lineNumber);
+                }
             }
         }
 
         HTTPError[ `_${i}` ] = class extends HTTPError[ baseClassName ] {
-            constructor(...args) {
-                super(...args);
+            constructor(message, fileName, lineNumber) {
+                super(message, fileName, lineNumber);
                 this.statusCode = statusCode;
             }
         };
