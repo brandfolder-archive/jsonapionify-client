4f54452c62b235252c55a5218586b073c3f82f8f
Jason Waldrip <jason@waldrip.net>
2016-02-01 09:34:30 -0700

    better error class names

--- errors.js
+++ errors.js
@@ -1,26 +1,28 @@
 "use strict";
 
-class HTTPError extends Error {
+const exports = {};
+
+exports.HTTPError = class extends Error {
     constructor(message, fileName, lineNumber) {
         super(message, fileName, lineNumber);
         this.name = this.constructor.name;
         Error.captureStackTrace(this, this.constructor.name)
     }
-}
+};
 
 for (var i = 400; i <= 599; i++) {
     (function (statusCode) {
-        var baseClassName = `_${(i - i % 100) / 100}xx`;
+        var baseClassName = `HTTPError${(i - i % 100) / 100}xx`;
 
         if (i % 100 == 0) {
-            HTTPError[ baseClassName ] = class extends HTTPError {
+            exports[ baseClassName ] = class extends HTTPError {
                 constructor(message, fileName, lineNumber) {
                     super(message, fileName, lineNumber);
                 }
             }
         }
 
-        HTTPError[ `_${i}` ] = class extends HTTPError[ baseClassName ] {
+        exports[ `HTTPError${i}` ] = class extends exports[ baseClassName ] {
             constructor(message, fileName, lineNumber) {
                 super(message, fileName, lineNumber);
                 this.statusCode = statusCode;
