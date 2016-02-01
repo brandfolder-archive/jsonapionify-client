9d6b00b8df58ea55e6ac8622f7d9c764138914a9
Jason Waldrip <jason@waldrip.net>
2016-02-01 09:55:20 -0700

    fix process response

--- helpers/process_response.js
+++ helpers/process_response.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const _ = require('lodash');
-const HTTPError = require('../errors.js');
+const errors = require('../errors.js');
 
 module.exports = function processResponse(response) {
     return new Promise(function (resolve, reject) {
@@ -19,7 +19,7 @@ module.exports = function processResponse(response) {
                 }
                 return msg
             }).join(', ');
-            reject(new HTTPError[ `_${response.status}` ](message), response);
+            reject(new errors[ `HTTPError${response.status}` ](message), response);
         } else {
             resolve(response);
         }
