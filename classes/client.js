5f483d6b5d5460801230f32626173d554c40d833
Jason Waldrip <jason@waldrip.net>
2016-02-05 10:55:00 -0700

    use jquery param

--- classes/client.js
+++ classes/client.js
@@ -7,88 +7,89 @@ var url = require('url');
 var http = require('http');
 var https = require('https');
 var ClientResponse = require('./client_response.js');
-var querystring = require('querystring');
 var _ = require('lodash');
+var parameterize = require('jquery-param');
 
 module.exports = class Client {
-    constructor(baseUrl, options) {
-        // Setup Headers
-        options = options || {};
-        this.headers = {};
-        this.headers["Content-Type"] = 'application/vnd.api+json';
-        this.headers["Accept"] = 'application/vnd.api+json';
-        _.extend(this.headers, options.headers || {});
+  constructor(baseUrl, options) {
+    // Setup Headers
+    options = options || {};
+    this.headers = {};
+    this.headers["Content-Type"] = 'application/vnd.api+json';
+    this.headers["Accept"] = 'application/vnd.api+json';
+    _.extend(this.headers, options.headers || {});
 
-        // Set baseUrl
-        this.baseUrl = baseUrl;
+    // Set baseUrl
+    this.baseUrl = baseUrl;
+  }
+
+  get(path, params, options) {
+    return this.request('GET', path, undefined, params, options);
+  }
+
+  post(path, data, params, options) {
+    return this.request('POST', path, data, params, options);
+  }
+
+  put(path, data, params, options) {
+    return this.request('PUT', path, data, params, options);
+  }
+
+  patch(path, data, params, options) {
+    return this.request('PATCH', path, data, params, options);
+  }
+
+  delete(path, data, params, options) {
+    return this.request('DELETE', path, data, params, options);
+  }
+
+  options(path, params, options) {
+    return this.request('OPTIONS', path, undefined, params, options);
+  }
+
+  request(method, path, data, params, options) {
+    path = path || '';
+    var requestUrl;
+
+    // Append params
+    if (params) {
+      path += `${path.match(/\?/) ? '&' : '?'}${parameterize(params)}`;
     }
 
-    get(path, params, options) {
-        return this.request('GET', path, undefined, params, options);
+    // Build the URL
+    if (url.parse(path).host) {
+      requestUrl = path;
+    } else if (path.indexOf(this.baseUrl) !== -1) {
+      requestUrl = path;
+    } else {
+      requestUrl = [this.baseUrl, path].map(function(string) {
+        return string.replace(/\/$/, '');
+      }).join('/');
     }
 
-    post(path, data, params, options) {
-        return this.request('POST', path, data, params, options);
+    // Build the options
+    options = {
+      method: method,
+      headers: this.headers
+    };
+
+    if (data) {
+      if (method.toLowerCase() == 'delete') {
+        options.headers['X-Http-Method-Override'] = method;
+        options.method = 'POST'
+      }
+      options.body = JSON.stringify(data)
     }
 
-    put(path, data, params, options) {
-        return this.request('PUT', path, data, params, options);
-    }
+    var response;
 
-    patch(path, data, params, options) {
-        return this.request('PATCH', path, data, params, options);
-    }
-
-    delete(path, data, params, options) {
-        return this.request('DELETE', path, data, params, options);
-    }
-
-    options(path, params, options) {
-        return this.request('OPTIONS', path, undefined, params, options);
-    }
-
-    request(method, path, data, params, options) {
-        path = path || '';
-        var requestUrl;
-
-        // Append params
-        if (params) {
-            path += `${path.match(/\?/) ? '&' : '?'}${querystring.stringify(params)}`;
-        }
-
-        // Build the URL
-        if (url.parse(path).host) {
-            requestUrl = path;
-        } else if (path.indexOf(this.baseUrl) !== -1) {
-            requestUrl = path;
-        } else {
-            requestUrl = [this.baseUrl, path].map(function (string) {
-                return string.replace(/\/$/, '');
-            }).join('/');
-        }
-
-        // Build the options
-        options = {
-            method: method,
-            headers: this.headers
-        };
-
-        if (data) {
-            if (method.toLowerCase() == 'delete') {
-                options.headers['X-Http-Method-Override'] = method;
-                options.method = 'POST'
-            }
-            options.body = JSON.stringify(data)
-        }
-
-        var response;
-
-        return fetch(requestUrl, options).then(function (res) {
-            response = new ClientResponse(res);
-            return res.text()
-        }).then(function (body) {
-            response.setBody(body);
-            return response;
-        })
-    }
-};
+    return fetch(requestUrl, options).then(function(res) {
+      response = new ClientResponse(res);
+      return res.text()
+    }).then(function(body) {
+      response.setBody(body);
+      return response;
+    })
+  }
+}
+;
